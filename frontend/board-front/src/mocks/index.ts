5627a78e87d3902da989269e95128191c7a0b997
JeongBanSeok <wibalislfkas@naver.com>
2024-10-31 04:29:52 +0900

    top3 게시물 리스트 컴포넌트 생성

--- frontend/board-front/src/mocks/index.ts
+++ frontend/board-front/src/mocks/index.ts
@@ -1,5 +1,7 @@
 import latestBoardListMock from "./latest-board-list.mock";
+import top3BoardListMock from "./top-3-board-list.mock";
 // 목 객체를 다른 곳에서 사용할 수 있도록 export
 export {
+  top3BoardListMock,
   latestBoardListMock,
 }
