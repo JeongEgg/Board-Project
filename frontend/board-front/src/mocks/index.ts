d55c2d09da177f6f772d331ea08fa3dfd936ddf5
JeongBanSeok <wibalislfkas@naver.com>
2024-10-31 15:47:12 +0900

    댓글 리스트 아이템, 좋아요 리스트 아이템 컴포넌트 생성

--- frontend/board-front/src/mocks/index.ts
+++ frontend/board-front/src/mocks/index.ts
@@ -1,7 +1,11 @@
 import latestBoardListMock from "./latest-board-list.mock";
 import top3BoardListMock from "./top-3-board-list.mock";
+import commentListMock from "./comment-list.mock";
+import favoriteListMock from "./favorite-list.mock";
 // 목 객체를 다른 곳에서 사용할 수 있도록 export
 export {
   top3BoardListMock,
   latestBoardListMock,
+  commentListMock,
+  favoriteListMock,
 }
